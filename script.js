e1cbb3b4afc648993c49c876b7ff9d2ed2cac179
MaciejMaciej7 <61986053+MaciejMaciej7@users.noreply.github.com>
2020-04-23 11:41:17 +0200

    Naprawiłem problem znikającego menu górnego

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 // JavaScript source code
 function myFunction() {
     var x = document.getElementById("menu_option");
-    if (x.style.display === "block") {
+    if (x.style.visibility === "initial") {
+        //x.style.display = "none";
+        x.style.visibility = "hidden";
         x.style.display = "none";
     } else {
+        x.style.visibility = "initial";
         x.style.display = "block";
     }
 }
+
+
+function CSSfunction(a){
+    if(a.matches) {
+    document.getElementById("menu_option").style.visibility = "initial";
+    document.getElementById("menu_option").style.display = "block";
+    }  
+}
+
+var res_state = window.matchMedia("(min-width: 1000px)")
+CSSfunction(res_state)
+res_state.addListener(CSSfunction)
