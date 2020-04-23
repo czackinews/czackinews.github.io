e7140651457bd252d6e7ae90967b963639d12f10
MaciejMaciej7 <61986053+MaciejMaciej7@users.noreply.github.com>
2020-04-23 19:04:04 +0200

    Wersji mobilnej część 2

--- script.js
+++ script.js
@@ -14,11 +14,25 @@ function myFunction() {
 
 function CSSfunction(a){
     if(a.matches) {
-    document.getElementById("menu_option").style.visibility = "initial";
-    document.getElementById("menu_option").style.display = "block";
-    }  
+        var menu_option = document.getElementById("menu_option")
+        menu_option.style.visibility = "initial";
+        menu_option.style.display = "block";
+
+        var thumbnail_col1 = document.getElementsByClassName("thumbnail-col1")[0]
+        thumbnail_col1.style.width = "33%";
+    }
+}
+
+function CSSfunction_mobile(b){
+    if (b.matches) {
+        var thumbnail_col1 = document.getElementsByClassName("thumbnail-col1")[0]
+        thumbnail_col1.style.width = "100%";
+    }
 }
 
 var res_state = window.matchMedia("(min-width: 1000px)")
+var res_state_mobile = window.matchMedia("(max-width: 999px)")
 CSSfunction(res_state)
+CSSfunction_mobile(res_state_mobile)
 res_state.addListener(CSSfunction)
+res_state_mobile.addListener(CSSfunction_mobile)
