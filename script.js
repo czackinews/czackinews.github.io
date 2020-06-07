7de2d92f0d2583d2145665194cdc8ba4388c4f57
MaciejMaciej7 <61986053+MaciejMaciej7@users.noreply.github.com>
2020-06-07 12:34:27 +0200

    Wersja mobilna część 3

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 // JavaScript source code
 function myFunction() {
     var x = document.getElementById("menu_option");
-    if (x.style.visibility === "initial") {
+    //if (x.style.visibility == "initial") {
+    if (x.style.display === "block") {
         //x.style.display = "none";
-        x.style.visibility = "hidden";
+        //x.style.visibility = "hidden";
         x.style.display = "none";
     } else {
-        x.style.visibility = "initial";
+        //x.style.visibility = "initial";
+        x.style.display = "block";
+    }
+
+}
+
+function plshelp(a) {
+    if(a.matches) {
+        var x = document.getElementById("menu_option");
         x.style.display = "block";
     }
 }
@@ -14,10 +23,16 @@ function myFunction() {
 
 function CSSfunction(a){
     if(a.matches) {
-        var menu_option = document.getElementById("menu_option")
-        menu_option.style.visibility = "initial";
-        menu_option.style.display = "block";
+        var x = document.getElementById("menu_option");
         
+        if (x.style.display === "none") {
+            alert("siet");
+            x.style.display = "block";
+        }
+        
+
+        //menu_option.style.visibility = "initial";
+
         //var thumbnail_photo = document.getElementsByClassName("thumbnail_photo")[0]
         //thumbnail_photo.style.width = "100%";
         //thumbnail_photo.style.float = "auto";
@@ -82,9 +97,12 @@ function CSSfunction_mobile(b){
     }
 }
 
-var res_state = window.matchMedia("(min-width: 1000px)")
-var res_state_mobile = window.matchMedia("(max-width: 999px)")
-CSSfunction(res_state)
-CSSfunction_mobile(res_state_mobile)
-res_state.addListener(CSSfunction)
-res_state_mobile.addListener(CSSfunction_mobile)
+var res_state = window.matchMedia("(min-width: 1100px)")
+//var res_state_mobile = window.matchMedia("(max-width: 999px)")
+//CSSfunction(res_state)
+//CSSfunction_mobile(res_state_mobile)
+//res_state.addListener(CSSfunction)
+//res_state_mobile.addListener(CSSfunction_mobile)
+
+plshelp(res_state)
+res_state.addListener(plshelp)
